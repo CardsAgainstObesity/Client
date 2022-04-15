42411f84e75ad3a9c4966685c4831efe0ce32bab
guskikalola <guskikalola@gmail.com>
2022-04-15 15:16:17 +0200

    Añadido event handler
    Añadido event handler para poder tener mas control sobre los eventos y poder juntarlos o hacer nuevos del lado del cliente

--- src/services/Room.mjs
+++ src/services/Room.mjs
@@ -1,10 +1,23 @@
+import EventHandler from './EventHandler.mjs';
+
 export default class Room {
 
     static _roomId = "";
     static _players = new Map();
     static _status = "lobby";
     static _czar = null;
-    
+
+    /**
+    * @type {EventHandler}
+    */
+    static _events = null;
+
+
+    static get events() {
+        if (this._events == null) this._events = new EventHandler();
+        return this._events;
+    }
+
 
     static get players() {
         return Room._players;
@@ -22,14 +35,20 @@ export default class Room {
     }
 
     static addPlayer(player) {
+        this.events.emit("PlayerAdded",player);
+        this.events.emit("PlayerListUpdated");
         Room._players.set(player.id, player);
     }
 
     static removePlayer(player) {
+        this.events.emit("PlayerRemoved",player);
+        this.events.emit("PlayerListUpdated");
         Room._players.delete(player.id);
     }
-    
+
     static setCzar(player) {
+        this.events.emit("CzarChanged",player);
+        this.events.emit("PlayerListUpdated");
         Room._czar = player;
     }
 
