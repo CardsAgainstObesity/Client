f750fb4d873685ddcc5813ccf918e196d1f92acc
Zlendy <argutierrezg11@gmail.com>
2022-05-31 21:39:20 +0200

    Migración de Composition API a Options API, preparativos para el cambio a componentes dinamicos

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,50 +1,69 @@
-<script setup>
+<script>
 import { game } from "@/services/cards.mjs";
-defineProps({
-  text: {
-    type: Object,
-    required: true
-  },
-  dark: {
-    type: Boolean,
-    required: true
-  },
-  clickable: {
-    type: Boolean,
-    required: true
-  },
-  active: {
-    type: Boolean,
-    required: false
-  }
-});
+export default {
+	data(){
+		return {
+			chapuza: `<span class='card_input'>[...]</span>`,
+		}
+	},
+    props: {
+        text: {
+            type: Object,
+            required: true,
+        },
+        dark: {
+            type: Boolean,
+            required: true,
+        },
+        clickable: {
+            type: Boolean,
+            required: true,
+        },
+        active: {
+            type: Boolean,
+            required: false,
+        },
+    },
+	methods: {
+		assignInput(value, clickable, language) {
+			if (clickable) {
+				if (
+					document.querySelectorAll(".card_input").length <= game.card_index
+				) {
+					game.card = 0;
+				} else {
+					document.querySelectorAll(".card_input")[
+						game.card_index
+					].innerHTML = value[language].replaceAll(".", "");
+					game.appendCardValue(value);
+					game.card_index++;
+				}
+			}
+		}
+	}
+};
 </script>
 
 <template>
-  <div @click="assignInput(text, clickable, $display.language)" :class="'game-card ' + (dark ? 'dark ' : ' ') + (clickable ? 'clickable ' : ' ') + (active ? 'active ' : ' ')">
-    <div class="game-card-inner">
-      <div class="game-card-front">
-      </div>
-      <div class="game-card-back" :style="active ? 'z-index: 0;' : 'z-index: -1;'">
-        <!-- Es necesario modificar el z-index para que html2canvas imprima la imagen correctamente. -->
-        <p v-html="text[$display.language].replaceAll(`___`, `${chapuza}`)" class="noselect"></p>
-      </div>
+    <div
+        @click="assignInput(text, clickable, $display.language)"
+        :class="
+            'game-card ' +
+            (dark ? 'dark ' : ' ') +
+            (clickable ? 'clickable ' : ' ') +
+            (active ? 'active ' : ' ')
+        "
+    >
+        <div class="game-card-inner">
+            <div class="game-card-front"></div>
+            <div
+                class="game-card-back"
+                :style="active ? 'z-index: 0;' : 'z-index: -1;'"
+            >
+                <!-- Es necesario modificar el z-index para que html2canvas imprima la imagen correctamente. -->
+                <p v-html="text[$display.language].replaceAll(`___`, `${chapuza}`)" class="noselect"></p>
+				<!-- <p v-html="'Creo que me olvidado ___ en ___.'.replaceAll(`___`, `${chapuza}`)" class="noselect"></p> -->
+            </div>
+        </div>
     </div>
-  </div>
 </template>
-
-<script>
-
-const chapuza = `<span class='card_input'>[...]</span>`;
-function assignInput(value, clickable, language) {
-    if (clickable){
-        if (document.querySelectorAll('.card_input').length <= game.card_index) {
-          game.card = 0;
-        } else {
-          document.querySelectorAll('.card_input')[game.card_index].innerHTML = value[language].replaceAll(".","");
-          game.appendCardValue(value);
-          game.card_index++;
-        }
-    }
-}
-</script>
