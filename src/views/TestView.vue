b51ee37574383d9ed0469903c7d9047ff21fedb7
Zlendy <argutierrezg11@gmail.com>
2022-06-04 14:16:40 +0200

    Pruebas de reproducción de audio usando Audio API

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,9 +1,30 @@
 <script>
 export default {
-    
+    data(){
+        return {
+            audio_array: {},
+        }
+    },
+    mounted(){
+        for(let index = 1; index <= 6; index++){
+            this.audio_array[index] = {
+                element: new Audio(`/audio/card_flip_${index}.mp3`),
+                name: `card_flip_${index}`,
+            }
+        }
+    },
+    methods: {
+        playAudioFlip(audio){
+            console.log("audio", audio);
+            audio.element.play();
+        }
+    }
 }
 </script>
 
 <template>
-    <h1>Test Audio API (Card flip sound)</h1>
+    <div>
+        <h1>Test Audio API (Card flip sound)</h1>
+        <button style="display: block;" v-for="audio in audio_array" :key="audio" @click="playAudioFlip(audio)">{{ audio.name }}</button>
+    </div>
 </template>
