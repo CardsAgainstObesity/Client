ad363390a1f76b40148889397c0afae5ac1cb85c
Zlendy <argutierrezg11@gmail.com>
2022-06-05 02:36:35 +0200

    Selector de volumen aplicado a los elementos de audio

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -3,20 +3,28 @@ export default {
     data(){
         return {
             audio_array: {},
+            audio_volume: 1,
         }
     },
     mounted(){
         for(let index = 1; index <= 6; index++){
             this.audio_array[index] = {
-                element: new Audio(`/audio/card_flip_${index}.mp3`),
                 name: `card_flip_${index}`,
+            }
+        }
+    },
+    watch: {
+        audio_volume(newvolume){
+            this.audio_volume = newvolume;
+            for (const element of document.querySelectorAll(".audio_card_flip")){
+                element.volume = newvolume;
             }
         }
     },
     methods: {
         playAudioFlip(audio){
             console.log("audio", audio);
-            audio.element.play();
+            document.getElementById(`audio_${audio.name}`).play();
         }
     }
 }
@@ -25,6 +33,12 @@ export default {
 <template>
     <div>
         <h1>Test Audio API (Card flip sound)</h1>
-        <button style="display: block;" v-for="audio in audio_array" :key="audio" @click="playAudioFlip(audio)">{{ audio.name }}</button>
+        <input type="range" v-model="audio_volume" min="0" max="1" step="0.1"/>
+            <ul>
+                <li v-for="(audio, index) in audio_array" :key="audio">
+                    <audio :id="`audio_${audio.name}`" class="audio_card_flip" :src="`/audio/card_flip_${index}.mp3`" controls />
+                    <button style="display: block;" @click="playAudioFlip(audio)">{{ audio.name }}</button>
+                </li>
+            </ul>
     </div>
 </template>
